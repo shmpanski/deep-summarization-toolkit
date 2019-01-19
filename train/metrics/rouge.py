9279708ec7933de838a4b059b1bad6557dc2175b
Andrey Sokolov <gooppe@yandex.ru>
2019-01-19 19:09:19 +0300

    Ignore empty sentences during evaluation

--- train/metrics/rouge.py
+++ train/metrics/rouge.py
@@ -13,10 +13,13 @@ class RougeMetric(Metric):
 
     def update(self, output):
         self._count += 1
-        rouge_res = self.rouge.get_scores(output[0], output[1], avg=True)
-        for metric, metric_val in rouge_res.items():
-            for stat, val in metric_val.items():
-                self._total_stats[metric][stat] += val
+        try:
+            rouge_res = self.rouge.get_scores(output[0], output[1], avg=True)
+            for metric, metric_val in rouge_res.items():
+                for stat, val in metric_val.items():
+                    self._total_stats[metric][stat] += val
+        except ValueError:
+            return
 
     def reset(self):
         self._total_stats = {metric: {stat: 0 for stat in self._stats} for metric in self._metrics}
