95dc88cab63a613a07b1df9382fb51e063fbcaa0
Andrey Sokolov <gooppe@yandex.ru>
2019-02-14 20:36:06 +0300

    Improve autoregressive_mask method

--- dst/nn/utils.py
+++ dst/nn/utils.py
@@ -1,11 +1,20 @@
+from typing import Tuple
+
 import torch
 
 
-def autoregressive_mask(tensor):
+def autoregressive_mask(batch_size: int, size: Tuple[int], device: torch.device = 'cpu', delay=1) -> torch.ByteTensor:
+    x = torch.ones(*size, device=device).triu(delay)
+
+    return x.repeat(batch_size, 1, 1).byte()
+
+
+def autoregressive_mask_like(tensor: torch.Tensor, delay=0) -> torch.ByteTensor:
     """Generate auto-regressive mask for tensor. It's used to preserving the auto-regressive property.
 
     Args:
         tensor (torch.Tensor): of shape ``(batch, seq_len)``.
+        delay (int): delay value.
 
     Returns:
         torch.Tensor: a byte mask tensor of shape ``(batch, seq_len, seq_len)`` containing mask for
@@ -13,7 +22,5 @@ def autoregressive_mask(tensor):
 
     """
     batch_size, seq_len = tensor.shape
-    x = torch.ones(
-        seq_len, seq_len, device=tensor.device).tril(-1).transpose(0, 1)
 
-    return x.repeat(batch_size, 1, 1).byte()
+    return autoregressive_mask(batch_size, (seq_len, seq_len), device=tensor.device, delay=delay)
