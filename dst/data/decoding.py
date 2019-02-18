3128b519ceaebce697e0d883ccea66fdd557a910
Andrey Sokolov <gooppe@yandex.ru>
2019-02-18 19:02:20 +0300

    Fix memory perfomance

--- dst/data/decoding.py
+++ dst/data/decoding.py
@@ -13,14 +13,14 @@ class BeamSearch:
 
     def __init__(self, k: int, device='cpu'):
         self.k = k
-        self.device = device
+        self.device = torch.device(device)
         self.scores = None
         self.sequences = None
 
     def initial_update(self, probs: torch.FloatTensor):
         scores = -torch.log(probs)
         top_scores, top_tokens = scores.topk(self.k)
-        self.sequences = [[token] for token in top_tokens]
+        self.sequences = [[token.item()] for token in top_tokens]
         self.scores = top_scores.view(self.k, 1)
 
     def update(self, probs: torch.FloatTensor):
@@ -39,6 +39,7 @@ class BeamSearch:
             assert probs.shape[0] == self.k, "Update must be done with k-beam prob distribution"
 
         probs_scores = self.scores - torch.log(probs)
+        probs_scores = probs_scores.detach().cpu()
         top_k_scores, top_k_tokens = probs_scores.topk(self.k)
         top_k_seq_idx = torch.arange(self.k).view(self.k, 1).repeat(1, self.k)
         top_k_scores, top_k_tokens, top_k_seq_idx = [t.view(-1) for t in [top_k_scores, top_k_tokens, top_k_seq_idx]]
@@ -48,10 +49,10 @@ class BeamSearch:
 
         _sequences = [[]] * self.k
         for i, seq_idx in enumerate(top_seq_idx):
-            _sequences[i] = self.sequences[seq_idx] + [top_tokens[i]]
+            _sequences[i] = self.sequences[seq_idx] + [top_tokens[i].item()]
 
         self.sequences = _sequences
-        self.scores = top_scores.view(self.k, 1)
+        self.scores = top_scores.view(self.k, 1).to(self.device)
 
     def search(self) -> torch.LongTensor:
         """Find best ``k`` sequence.
@@ -60,4 +61,4 @@ class BeamSearch:
             torch.LongTensor: Decoded sequences.
         """
 
-        return torch.LongTensor(self.sequences, device=self.device)
+        return torch.LongTensor(self.sequences).to(self.device)
