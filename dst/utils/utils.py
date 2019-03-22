0dc996f6005db9dff44abf5613122c1a77f7a6af
Andrey Sokolov <gooppe@yandex.ru>
2019-03-22 18:34:59 +0300

    Reformating

--- dst/utils/utils.py
+++ dst/utils/utils.py
@@ -5,7 +5,7 @@ import tqdm
 import yaml
 
 
-class TqdmLoggingHandler (logging.Handler):
+class TqdmLoggingHandler(logging.Handler):
     def __init__(self, level=logging.NOTSET):
         super(self.__class__, self).__init__(level)
 
@@ -20,13 +20,15 @@ class TqdmLoggingHandler (logging.Handler):
             self.handleError(record)
 
 
-def setup_logging(default_path='logging.yml', default_level=logging.INFO, env_key='LOG_CFG'):
+def setup_logging(
+    default_path="logging.yml", default_level=logging.INFO, env_key="LOG_CFG"
+):
     path = default_path
     value = os.getenv(env_key, None)
     if value:
         path = value
     if os.path.exists(path):
-        with open(path, 'rt') as f:
+        with open(path, "rt") as f:
             config = yaml.safe_load(f.read())
         logging.config.dictConfig(config)
     else:
